aed941114faeba9afd38ee36a051c5368544d454
David Palm <dvdplm@gmail.com>
2025-08-05 16:02:25 +0200

    basic launch and stay resident app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,81 @@
-mod cocoa_app;
+//! Implementing `NSApplicationDelegate` for a custom class.
+#![deny(unsafe_op_in_unsafe_fn)]
+use objc2::rc::Retained;
+use objc2::runtime::ProtocolObject;
+use objc2::{DefinedClass, MainThreadMarker, MainThreadOnly, define_class, msg_send};
+use objc2_app_kit::{NSApplication, NSApplicationActivationPolicy, NSApplicationDelegate};
+use objc2_foundation::{
+    NSCopying, NSNotification, NSObject, NSObjectProtocol, NSString, ns_string,
+};
+
+#[derive(Debug)]
+#[allow(unused)]
+struct Ivars {
+    ivar: u8,
+    another_ivar: bool,
+    box_ivar: Box<i32>,
+    maybe_box_ivar: Option<Box<i32>>,
+    id_ivar: Retained<NSString>,
+    maybe_retained_ivar: Option<Retained<NSString>>,
+}
+
+define_class!(
+    // SAFETY:
+    // - The superclass NSObject does not have any subclassing requirements.
+    // - `AppDelegate` does not implement `Drop`.
+    #[unsafe(super(NSObject))]
+    #[thread_kind = MainThreadOnly]
+    #[ivars = Ivars]
+    struct AppDelegate;
+
+    unsafe impl NSObjectProtocol for AppDelegate {}
+
+    unsafe impl NSApplicationDelegate for AppDelegate {
+        #[unsafe(method(applicationDidFinishLaunching:))]
+        fn did_finish_launching(&self, notification: &NSNotification) {
+            println!("Did finish launching!");
+            println!("Process ID: {}", std::process::id());
+            // Do something with the notification
+            dbg!(notification);
+            // Access instance variables
+            dbg!(self.ivars());
+
+            NSApplication::main(MainThreadMarker::from(self));
+        }
+
+        #[unsafe(method(applicationWillTerminate:))]
+        fn will_terminate(&self, _notification: &NSNotification) {
+            println!("Will terminate!");
+        }
+    }
+);
+
+impl AppDelegate {
+    fn new(ivar: u8, another_ivar: bool, mtm: MainThreadMarker) -> Retained<Self> {
+        let this = Self::alloc(mtm);
+        let this = this.set_ivars(Ivars {
+            ivar,
+            another_ivar,
+            box_ivar: Box::new(2),
+            maybe_box_ivar: None,
+            id_ivar: NSString::from_str("abc"),
+            maybe_retained_ivar: Some(ns_string!("def").copy()),
+        });
+        unsafe { msg_send![super(this), init] }
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let mtm: MainThreadMarker = MainThreadMarker::new().unwrap();
+
+    let app = NSApplication::sharedApplication(mtm);
+    app.setActivationPolicy(NSApplicationActivationPolicy::Regular);
+
+    // configure the application delegate
+    let delegate = AppDelegate::new(42, true, mtm);
+    let object = ProtocolObject::from_ref(&*delegate);
+    app.setDelegate(Some(object));
+
+    // run the app
+    app.run();
 }
